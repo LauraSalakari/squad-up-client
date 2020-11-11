159d509e5dc6c82251131599b311da87b2f34b18
Laura Salakari <salakari.laura@gmail.com>
2020-11-11 22:59:54 +0100

    no message

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const API_URL = "https://squad-u-p.herokuapp.com/api" 
-export const API_URL = "http://localhost:5000/api"
+export const API_URL = "https://squad-u-p.herokuapp.com/api" 
+//export const API_URL = "http://localhost:5000/api"
 export const RAWG_API_KEY = "0d8d5106350244dd979ecfa10b19036d"
