990f2534d8d9eeb099b96fe7cd866756cfc5d18a
Laura Salakari <salakari.laura@gmail.com>
2020-11-11 22:10:49 +0100

    squad display

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,14 +1,21 @@
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { API_URL } from "../config";
+import { Link } from "react-router-dom"
+import {GiShuriken} from "react-icons/gi"
 
 export default function ProfilePage(props) {
     const [user, setUser] = useState(null)
+    const [squads, setSquads] = useState(null)
 
     useEffect(() => {
         Axios.get(`${API_URL}/profile/${props.match.params.id}`, { withCredentials: true })
             .then((response) => {
                 setUser(response.data)
+                Axios.get(`${API_URL}/profile/${props.match.params.id}/squads`, { withCredentials: true })
+                    .then((response) => {
+                        setSquads(response.data);
+                    })
             })
             .catch((err) => {
                 console.log(err)
@@ -51,17 +58,36 @@ export default function ProfilePage(props) {
                     {
                         (user.games) ? (
                             user.games.map((elem) => {
-                            let parsed = JSON.parse(elem);
-                            return <div key={parsed.id}>
-                                <img src={parsed.background_image} alt="Game" style={{ height: 100 }} />
-                                {parsed.name}
-                            </div>
-                        })
+                                let parsed = JSON.parse(elem);
+                                return <div key={parsed.id}>
+                                    <img src={parsed.background_image} alt="Game" style={{ height: 100 }} />
+                                    {parsed.name}
+                                </div>
+                            })
                         ) : <p>No games specified</p>
-
-                        
                     }
                 </p>
+                <div>
+                    <h4>{user.username}'s Squads</h4>
+                    {
+                        (squads) ? (
+                            squads.map((elem) => {
+                                if (elem.creator._id === user._id) {
+                                    return <div key={elem._id}>
+                                        <h6>{elem.title}</h6>
+                                        <p>Creator: <b><Link to={elem.creator._id}>{elem.creator.username}</Link></b> <GiShuriken style={{color: "9800ff"}}/> </p>
+                                    </div>
+                                }
+                                else {
+                                    return <div key={elem._id}>
+                                        <h6>{elem.title}</h6>
+                                        <p>Creator: <b><Link to={elem.creator._id}>{elem.creator.username}</Link> </b></p>
+                                    </div>
+                                }
+                            })
+                        ) : (null)
+                    }
+                </div>
             </div>
         )
     }
